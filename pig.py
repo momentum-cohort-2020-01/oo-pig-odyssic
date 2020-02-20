1b6cb591354148ef81772da6322fa40851a40350
odyssic <stevenbuddenjr@gmail.com>
2020-02-20 09:31:16 -0500

    first commits

--- pig.py
+++ pig.py
@@ -7,14 +7,13 @@ DEALERS = ["Portbelly", "Stewy", "Fragonaard",
 
 
 class Die:
-    def __init__(self, value):
-        self.value = value
-    pass
+    def __init__(self):
+        self.sides = DIE
 
 
 class Player:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        # self.name = name
         self.turn_score = []
         self.score = 0
 
@@ -27,68 +26,72 @@ class Dealer:
 
 
 class Game:
-    def __init__(self, player, dealer, die):
-        self.player = player
-        self.dealer = dealer
-        self.die = die
-    pass
 
-    def __str__(self):
-        print
+    def __init__(self):
+        self.player = Player()
+        self.dealer = Dealer()
+        self.die = Die()
 
-    def roll(self, participant):
-        # def __init__:
-        print('Rolling the die...')
-        roll = random.choice(DIE)
-        return roll
-        if self.participant is self.player:
-            print(f'You rolled a {roll}')
-            turn_score = turn_score(player.turn_score, roll)
-
-        if self.participant is self.dealer:
-            print(f'Dealer rolled a {roll}')
-            turn_score = turn_score(dealer.turn_score, roll)
-
-    def initial_roll(self):
-        roll = random.choice(DIE)
-        return roll
-        print(f'You rolled a {roll}')
-
-    def turn_score(self, turn_score, roll):
-        turn_score.append(int(roll))
-        sum(turn_score)
-
-    def game_score(self, player, score):
-        self.score
-
+    # def __str__(self):
+    #     print
     def print_rules(self):
+        breakpoint()
         pig_rules = "'Notorious P.I.G.' is a game of 'chance' (a.k.a DESTINY)\nThe rules are simple: the first entity to reach 100 wins!\nOn your turn, you roll a 6-sided die as many times as you like. \nYou are able to rack up points on each turn. \n If you roll a '1', you lose the points for that turn. \nIf you choose to 'hold', you keep your points, and your digitized opponent rolls. \nShe will roll until her score is at least twenty, \nor she rolls a '1', and then the proverbial die passes back to you."
 
         print(pig_rules)
 
         print("Are you ready to begin?: ")
-        self.startgame()
+        #  self.startgame()
 
-    def startgame(self):
-        answer = input('"(Y)es" or "(No)"?')
+    # def startgame(self):
 
-        if answer is "Y".lower():
-            while name == '':
-                name = input("Exciting! Please enter your name to proceed: ")
-                greeting = f"Welcome to the game {name}! You and your opponent will now each roll to determine who goes first! The lower number goes first."
+    #     answer = input('"(Y)es" or "(No)"?')
 
-                print(greeting)
+    #     if answer is "Y".lower():
+    #         while name == '':
+    #             name = input(
+    #                 "Exciting! Please enter your name to proceed: ")
+    #             greeting = f"Welcome to the game {name}! You and your opponent will now each roll to determine who goes first! The lower number goes first."
 
-            self.initial_roll()
+    #             print(greeting)
 
-        if answer is "N".lower():
-            print(
-                "Please return when you're ready to play dice with the Universe!\nThis game may self destruct!")
+    #         self.initial_roll()
 
-            end_game()
+    #     if answer is "N".lower():
+    #         print(
+    #             "Please return when you're ready to play dice with the Universe!\nThis game may self destruct!")
 
-        def end_game(self):
-            pass
+    #         end_game()
+
+    # def roll(self, participant):
+    #     # def __init__:
+    #     print('Rolling the die...')
+    #     roll = random.choice(DIE)
+    #     return roll
+    #     if self.participant is self.player:
+    #         print(f'You rolled a {roll}')
+    #         turn_score = turn_score(player.turn_score, roll)
+
+    #     if self.participant is self.dealer:
+    #         print(f'Dealer rolled a {roll}')
+    #         turn_score = turn_score(dealer.turn_score, roll)
+
+    # def initial_roll(self):
+    #     roll = random.choice(DIE)
+    #     return roll
+    #     print(f'You rolled a {roll}')
+
+    # def turn_score(self, turn_score, roll):
+    #     turn_score.append(int(roll))
+    #     sum(turn_score)
+
+    # def game_score(self, player, score):
+    #     self.score
+
+    #     def end_game(self):
+    #         pass
 
 
-Game(Player, Dealer, Die)
+game = Game()
+
+game.print_rules()
