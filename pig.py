83fd2d40f090b8d1d95f2b396e4a85f59f2b638d
odyssic <stevenbuddenjr@gmail.com>
2020-02-19 19:21:56 -0500

    getting some functions in

--- pig.py
+++ pig.py
@@ -1,13 +1,9 @@
 import random
 
 DIE = [1, 2, 3, 4, 5, 6]
-PLAYER = []
+# PLAYERS = [name, DEALERS]
 DEALERS = ["Portbelly", "Stewy", "Fragonaard",
            "Tim the Usurper", "Lilian Lovelace"]
-
-# player is DEALER or PLAYER?
-
-Game()
 
 
 class Die:
@@ -19,7 +15,8 @@ class Die:
 class Player:
     def __init__(self, name):
         self.name = name
-    pass
+        self.turn_score = []
+        self.score = 0
 
 
 class Dealer:
@@ -27,8 +24,6 @@ class Dealer:
         self.name = random.choice(DEALERS)
         self.turn_score = []
         self.score = 0
-
-    pass
 
 
 class Game:
@@ -41,40 +36,50 @@ class Game:
     def __str__(self):
         print
 
-    def roll(self):
-        def __init__:
-        return roll = random.choice(DIE)
-        print(f'You rolled a {roll}')
+    def roll(self, participant):
+        # def __init__:
+        print('Rolling the die...')
+        roll = random.choice(DIE)
+        return roll
+        if self.participant is self.player:
+            print(f'You rolled a {roll}')
+            turn_score = turn_score(player.turn_score, roll)
+
+        if self.participant is self.dealer:
+            print(f'Dealer rolled a {roll}')
+            turn_score = turn_score(dealer.turn_score, roll)
 
     def initial_roll(self):
-        return roll = random.choice(DIE)
+        roll = random.choice(DIE)
+        return roll
         print(f'You rolled a {roll}')
 
-    def turn_score(self, roll):
+    def turn_score(self, turn_score, roll):
         turn_score.append(int(roll))
         sum(turn_score)
 
-    def game_score(self, player, score)
-    self.score
+    def game_score(self, player, score):
+        self.score
 
     def print_rules(self):
-        pig_rules = "Notorious P.I.G. is a game of 'chance' (a.k.a DESTINY)\nThe rules are simple: the first entity to reach 100 wins!\nOn your turn, you roll a 6-sided die as many times as you like. \nYou are able to rack up points on each turn. \n If you roll a '1', you lose the points for that turn. \nIf you choose to 'hold', you keep your points, and your digitized opponent rolls. \nShe will roll until her score is at least twenty, \nor she rolls a '1', and then the proverbial die passes back to you."
+        pig_rules = "'Notorious P.I.G.' is a game of 'chance' (a.k.a DESTINY)\nThe rules are simple: the first entity to reach 100 wins!\nOn your turn, you roll a 6-sided die as many times as you like. \nYou are able to rack up points on each turn. \n If you roll a '1', you lose the points for that turn. \nIf you choose to 'hold', you keep your points, and your digitized opponent rolls. \nShe will roll until her score is at least twenty, \nor she rolls a '1', and then the proverbial die passes back to you."
 
         print(pig_rules)
 
         print("Are you ready to begin?: ")
-        startgame()
+        self.startgame()
 
     def startgame(self):
         answer = input('"(Y)es" or "(No)"?')
 
         if answer is "Y".lower():
-            print("Exciting! Please enter your name to proceed: ")
-            name = input("Name")
-            greeting = f"Welcome to the game {name}! You and your opponent will now each roll to determine who goes first! The lower number goes first."
-            print(greeting)
+            while name == '':
+                name = input("Exciting! Please enter your name to proceed: ")
+                greeting = f"Welcome to the game {name}! You and your opponent will now each roll to determine who goes first! The lower number goes first."
 
-            roll()
+                print(greeting)
+
+            self.initial_roll()
 
         if answer is "N".lower():
             print(
@@ -82,5 +87,8 @@ class Game:
 
             end_game()
 
-    def end_game(self):
-    quit
+        def end_game(self):
+            pass
+
+
+Game(Player, Dealer, Die)
